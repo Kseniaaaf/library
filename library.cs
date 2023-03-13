d622ae7af895e6280bd6f61048951b6350bc1a56
Kseniaaaf <kseniafeduro@gmail.com>
2023-03-13 15:51:37 +0300

    First Commit

---
+++ library.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @class
+{
+     class library
+{
+        public string Title { get; set; }
+        public double Address { get; set; }
+        public double YearOfOpening { get; set; }
+
+        public library() { }
+        public library(string title, double address, double yearofopening)
+        {
+            Title = title;
+            Address = address;
+            YearOfOpening = yearofopening;
+        }
+            }
+}
